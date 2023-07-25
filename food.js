8fddad5850c0dd74978e90e1924a2451619ec5c9
Henryfrank <henryfrank61@yahoo.com>
2023-07-25 23:59:11 +0100

    added js

--- food.js
+++ food.js
@@ -13,10 +13,29 @@
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirm_password").value;
     var mail = document.getElementById("email").value;
-   if ()
     if (password == confirmPassword) {
         alert("Registration successful!");
     } else {
         alert("Password does not match!")
     }
 }
+function refer(event) {
+event.preventDefault();
+
+          var email = document.getElementById("username").value;
+          var password = document.getElementById("pword").value;
+
+          if (email === "admin@gmail.com" && password === "user") {
+               window.location.replace("orderpg.html");
+          } else {
+              alert("Invalid information");
+              return;
+          }
+    }
+function login_form_submit()
+{
+  var farmerID=document.getElementById("customer").value;
+  localStorage.setItem("id", farmerID);
+  return false;
+}
+document.getElementById("farmer").innerHTML=localStorage.getItem("id");
